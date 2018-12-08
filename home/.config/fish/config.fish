ad7e005f71b5f4ca01e0fcc07854b949c00bf6f4
Nick Dowdell <nick.dowdell@nike.com>
2018-12-07 17:42:34 -0800

    add get / GET / put / PUT / post / POST / delete / DELETE fish functions

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -6,3 +6,28 @@ if not set -q abbrs_initialized
   abbr vim  'nvim'
   set -U abbrs_initialized
 end
+
+function get --wraps http
+  http GET $argv Authorization:$JWT
+end
+function GET --wraps http
+  http -v GET $argv Authorization:$JWT
+end
+function put --wraps http
+  http PUT $argv Authorization:$JWT
+end
+function PUT --wraps http
+  http -v PUT $argv Authorization:$JWT
+end
+function post --wraps http
+  http POST $argv Authorization:$JWT
+end
+function POST --wraps http
+  http -v POST $argv Authorization:$JWT
+end
+function delete --wraps http
+  http DELETE $argv Authorization:$JWT
+end
+function DELETE --wraps http
+  http -v DELETE $argv Authorization:$JWT
+end
